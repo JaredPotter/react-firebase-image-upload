1cabcfd88fd7f41b790206d646bdeacc3f8fd31d
jaredpotter <jaredpotter1@gmail.com>
2020-08-27 09:22:04 -0600

    Add image change option

--- src/App.js
+++ src/App.js
@@ -16,10 +16,16 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 
 function App() {
+  const [existingImageUrl, setExistingImageUrl] = React.useState(
+    "https://firebasestorage.googleapis.com/v0/b/react-firebase-image-upl-53de3.appspot.com/o/images%2FScreen%20Shot%202020-06-15%20at%209.09.13%20PM%20(2).png?alt=media&token=cfd9b764-6c6f-4fc4-ab08-34750a0c4037"
+  );
+  const [isLoading, setIsLoading] = React.useState(false);
+
   const inputFileElement = React.useRef(null);
 
   function handleUploadClick() {
     const file = inputFileElement.current.files[0];
+    setIsLoading(true);
 
     const storageRef = firebase.storage().ref();
     const fileRef = storageRef.child(`images/${file.name}`);
@@ -27,18 +33,34 @@ function App() {
       console.log("Uploaded a blob or file!");
       snapshot.ref.getDownloadURL().then((downloadURL) => {
         console.log("File available at", downloadURL);
+        setExistingImageUrl(downloadURL);
+        setIsLoading(false);
       });
     });
   }
 
+  function handleImageChange() {
+    setExistingImageUrl("");
+  }
+
   return (
     <div className="App">
-      <input
-        type="file"
-        ref={inputFileElement}
-        accept="image/png, image/jpeg"
-      />
-      <button onClick={handleUploadClick}>Upload</button>
+      {isLoading ? <div>UPLOADING...</div> : null}
+      {existingImageUrl ? (
+        <>
+          <img src={existingImageUrl} className="image-preview" />
+          <button onClick={handleImageChange}>Change</button>
+        </>
+      ) : (
+        <>
+          <input
+            type="file"
+            ref={inputFileElement}
+            accept="image/png, image/jpeg"
+          />
+          <button onClick={handleUploadClick}>Upload</button>
+        </>
+      )}
     </div>
   );
 }
